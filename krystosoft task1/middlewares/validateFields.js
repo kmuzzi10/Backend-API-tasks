941adc760cada1498defe895d89fcc0744557289
kmuzzi10 <kmuzammil12546@gmail.com>
2024-08-22 23:13:09 +0500

    complete

--- krystosoft task1/middlewares/validateFields.js
+++ krystosoft task1/middlewares/validateFields.js
@@ -1,4 +1,8 @@
-export const validateNoExtraFields = (allowedFields) => (req, res, next) => {
+import { body } from 'express-validator';
+
+const allowedFields = ['name', 'description', 'price'];
+
+export const validateNoExtraFields = (req, res, next) => {
     const extraFields = Object.keys(req.body).filter(field => !allowedFields.includes(field));
 
     if (extraFields.length > 0) {
@@ -9,3 +13,35 @@ export const validateNoExtraFields = (allowedFields) => (req, res, next) => {
     }
     next();
 };
+
+export const validateItemCreation = [
+    body('name')
+        .notEmpty().withMessage('Name is required')
+        .isString().withMessage('Name must be a string')
+        .matches(/^[A-Za-z\s]+$/).withMessage('Name should contain alphabetic characters only'),
+    body('description')
+        .notEmpty().withMessage('Description is required')
+        .isString().withMessage('Description must be a string')
+        .matches(/^[A-Za-z\s]+$/).withMessage('Description should contain alphabetic characters only'),
+    body('price')
+        .notEmpty().withMessage('Price is required')
+        .isFloat({ gt: 0 }).withMessage('Price must be greater than zero'),
+    validateNoExtraFields
+];
+
+export const validateItemUpdate = [
+    body('name')
+        .optional()
+        .isString().withMessage('Name must be a string')
+        .matches(/^[A-Za-z\s]+$/).withMessage('Name should contain alphabetic characters only'),
+    body('description')
+        .optional()
+        .isString().withMessage('Description must be a string')
+        .matches(/^[A-Za-z\s]+$/).withMessage('Description should contain alphabetic characters only'),
+    body('price')
+        .optional()
+        .isFloat({ gt: 0 }).withMessage('Price must be greater than zero'),
+    validateNoExtraFields
+];
+
+
